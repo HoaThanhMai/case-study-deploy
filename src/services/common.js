7a81cda148dc8fa81e4ae4c253a6131b6ab258ea
khoitv4 <khoitv4>
2023-12-13 05:29:12 +0700

    change json-server api

--- src/services/common.js
+++ src/services/common.js
@@ -1,3 +1,3 @@
-export const CATEGORY_API_URL = 'https://jsonserver-vercel-api.vercel.app/categories'
-export const COMPANY_API_URL = 'https://jsonserver-vercel-api.vercel.app/companies'
-export const COLOR_API_URL = 'https://jsonserver-vercel-api.vercel.app/colors'
+export const CATEGORY_API_URL = 'https://json-server-api-rj2309r1.vercel.app/categories'
+export const COMPANY_API_URL = 'https://json-server-api-rj2309r1.vercel.app/companies'
+export const COLOR_API_URL = 'https://json-server-api-rj2309r1.vercel.app/colors'
